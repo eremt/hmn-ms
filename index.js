54e7dd9793984eae7bdde5a4887ecc584a66639a
User <eremtroy@gmail.com>
2019-11-14 17:06:35 +0100

    define units

---
+++ index.js
@@ -0,0 +1,31 @@
+const s = 1000
+const m = 60 * s
+const h = 60 * m
+const d = 24 * h
+const y = 365.25 * d
+
+const units = {
+  s,
+  sec,
+  secs,
+  second,
+  seconds,
+
+  m,
+  min,
+  mins,
+  minute,
+  minutes,
+
+  h,
+  hour,
+  hours,
+
+  d,
+  day,
+  days,
+
+  y,
+  year,
+  years
+}
